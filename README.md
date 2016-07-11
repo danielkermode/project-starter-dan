e28193852073192aa8e53b31eccff30cba4c549c
danielkermode <danielkermode@hotmail.co.nz>
2016-07-11 22:26:15 +1200

    semi working

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 A scaffolding tool for building node projects.
 
+```npm i -g project-starter-dan```.
+
 Usage:
+```project-starter <template> <yourdirname>```.
+
+A directory will be created (within the current directory) with the given name.
+
+Template is one of:
+
+```barebones-react``` for a basic react setup.
 
 WIP.
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+
 const ncp = require('ncp').ncp;
 
 ncp.limit = 16;
 
 function copyTemplate(name, dest) {
-  ncp(__dirname + '/templates/' + name, __dirname + '/' + dest, (err) => {
+  ncp(__dirname + '/templates/' + name, process.cwd() + '/' + dest, (err) => {
     if(err) {
       return console.error(err);
     }
@@ -11,13 +13,11 @@ function copyTemplate(name, dest) {
   });
 }
 
-
 const args = process.argv;
-console.log(args)
 
 if(!args[2] || !args[3]) {
-  console.log(' usage:');
-  console.log('  cmd <template> <yourdirname>');
+  console.log('   usage:\n');
+  console.log('     project-starter <template> <yourdirname>');
 } else {
   copyTemplate(args[2], args[3]);
 }
