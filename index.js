b83ec9dcb0b1ca7e82504a720f4441b8639b86e1
danielkermode <danielkermode@hotmail.co.nz>
2016-07-13 00:10:52 +1200

    another try

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const lib = require('./lib/utils');
 const args = process.argv;
 const templates = lib.getDirectories(path.join(__dirname, 'templates'));
 
+console.log(args)
+
 function argHandler(templates, args) {
   if(!templates.includes(args[2])) {
     switch(args[2]) {
       case 'add':
         if(args[3]) {
-          lib.saveTemplate(args[3], args[1], process.cwd());
+          lib.saveTemplate(args[3], process.cwd(), __dirname);
         } else {
           help();
         }
@@ -23,7 +25,7 @@ function argHandler(templates, args) {
     }
   } else {
     // template requested exists, copy template into either given name or default name
-    const templateDir = path.join(args[1], 'templates', args[2]);
+    const templateDir = path.join(__dirname, 'templates', args[2]);
     const destDir = path.join(process.cwd(), args[3] ? args[3] : args[2]);
     lib.copyTemplate(templateDir, destDir);
   }
@@ -56,7 +58,7 @@ if(!args[2] && !args[3]) {
       message: 'What\'s your project called?'
     }
   ]).then((answers) => {
-    const templateDir = path.join(args[1], 'templates', answers.template);
+    const templateDir = path.join(__dirname, 'templates', answers.template);
     const destDir = path.join(process.cwd(), answers.projName);
     lib.copyTemplate(templateDir, destDir);
   });
